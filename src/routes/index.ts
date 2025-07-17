26095f7424d83e9c4652e1ae8c1035cbd847b4e0
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-17 14:52:10 +0600

    admin route added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AuthRoutes } from '../app/modules/auth/auth.route';
 import { UserRoutes } from '../app/modules/user/user.route';
+import { AdminRouter } from '../app/modules/admin/admin.route';
 const router = express.Router();
 
 const apiRoutes = [
@@ -11,6 +12,10 @@ const apiRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/admin',
+    route: AdminRouter,
   },
 ];
 
